41aea64ee7743d6eba51077c997e4f8a75c9df65
JamieTanJiahui <jamietan888@gmail.com>
2025-10-27 15:15:00 +0800

    fix: fixed projects on backend

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -5,8 +5,6 @@ const ProjectSchema = new Schema({
   name: { type: String, required: true, trim: true },
   department: [{ type: Types.ObjectId, ref: "Department", required: true }],
   description: { type: String, default: '' },
-  department: [{ type: Types.ObjectId, ref: 'Department', default: null, index: true}],
-  deadline: { type: Date },
   createdBy: { type: Types.ObjectId, ref: 'User', required: true, index: true },
   teamMembers: [{ type: Types.ObjectId, ref: 'User', default: [], index: true }]
 },
